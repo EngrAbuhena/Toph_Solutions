dc9d5d8ddbdc2d581f434dd0f7ddde2cdb4af9b7
Abuhena Rony <23054213+EngrAbuhena@users.noreply.github.com>
2022-03-28 01:36:40 +0600

    Add files via upload

---
+++ MixedFractions.cpp
@@ -0,0 +1,17 @@
+// Mixed Fractions
+// Accepted
+// Author: Abuhena Rony
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2; // numerator
+    int den;        // denominator
+    int rem;        // reminder
+    cin >> num1 >> den;
+    rem = num1 % den;
+    num2 = num1 / den;
+    cout << num2 << " " << rem << " " << den;
+    return 0;
+}
