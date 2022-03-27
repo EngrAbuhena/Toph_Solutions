50e6e01ac55f476c6cb0453f103da238b50e20e0
Abuhena Rony <23054213+EngrAbuhena@users.noreply.github.com>
2022-03-28 01:34:26 +0600

    Formatted Document

--- TheTitan.cpp
+++ TheTitan.cpp
@@ -4,15 +4,17 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int number;
-    cin>>number;
-    int result=0;
-    int x=1;
-    while(x<=number){
-        result+=(x*x);
+    cin >> number;
+    int result = 0;
+    int x = 1;
+    while (x <= number)
+    {
+        result += (x * x);
         x++;
     }
-    cout<<result;
+    cout << result;
     return 0;
 }
