ded0c95e925f0c82f2d35b8874e68dd6402341d2
Abuhena Rony <23054213+EngrAbuhena@users.noreply.github.com>
2022-03-28 01:37:12 +0600

    Add files via upload

---
+++ MathandWatermelons.cpp
@@ -0,0 +1,14 @@
+// Math and Watermelons
+// Accepted
+// Author: Abuhena Rony
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int m; // watermelons
+    int k; // number of people
+    cin >> m >> k;
+    cout << m % k;
+    return 0;
+}
