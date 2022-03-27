0a852210217aed79d931abf2979f273be38a1d74
Abuhena Rony <23054213+EngrAbuhena@users.noreply.github.com>
2022-03-28 01:32:52 +0600

    Add files via upload

---
+++ TheTitan.cpp
@@ -0,0 +1,18 @@
+// The Titan
+// Accepted
+// Author: Abuhena Rony
+#include <iostream>
+using namespace std;
+
+int main(){
+    int number;
+    cin>>number;
+    int result=0;
+    int x=1;
+    while(x<=number){
+        result+=(x*x);
+        x++;
+    }
+    cout<<result;
+    return 0;
+}
