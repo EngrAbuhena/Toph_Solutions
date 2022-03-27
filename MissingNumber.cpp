35cb8336e7d110d656126103ef5f9b0eb8cf49b2
Abuhena Rony <23054213+EngrAbuhena@users.noreply.github.com>
2022-03-28 02:38:49 +0600

    Create MissingNumber.cpp

---
+++ MissingNumber.cpp
@@ -0,0 +1,13 @@
+// Missing Number
+// Accepted
+// Author: Abuhena Rony
+#include <iostream>
+
+int main()
+{
+    int sum, a, b, c;
+    std::cin >> sum;
+    std::cin >> a >> b >> c;
+    std::cout << sum - (a + b + c);
+    return 0;
+}
