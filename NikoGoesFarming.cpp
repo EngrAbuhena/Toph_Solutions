bc77c929c7b994cf2f3eaf1f21b63403e10da9b6
Abuhena Rony <23054213+EngrAbuhena@users.noreply.github.com>
2022-04-01 18:11:29 +0600

    Create NikoGoesFarming.cpp

---
+++ NikoGoesFarming.cpp
@@ -0,0 +1,29 @@
+// Niko Goes Farming
+// Accepted
+// Author: Abuhena Rony
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int lands;
+    int perimeter;
+    int temp;
+
+    cin >> lands; // taking input for lands
+
+    for (int x = 1; x <= lands; x++)
+    {
+        cin >> perimeter; // taking perimeter input for every lands
+        if (perimeter % 4 == 0)
+        {
+            temp = perimeter / 4;
+            cout << temp * temp << endl;
+        }
+        else
+        {
+            temp = perimeter;
+        }
+    }
+    return 0;
+}
