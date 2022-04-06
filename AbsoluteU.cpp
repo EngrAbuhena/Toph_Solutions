3f8d10cf809636a19d51e4d82e8219b59eab742b
Abuhena Rony <23054213+EngrAbuhena@users.noreply.github.com>
2022-04-06 07:44:12 +0600

    Create AbsoluteU.cpp

---
+++ AbsoluteU.cpp
@@ -0,0 +1,21 @@
+// Absolute U
+// Accepted
+// Author: Abuhena Rony
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin >> testCase;
+    int number;
+    for (int x = 1; x <= testCase; x++)
+    {
+        cin >> number;
+        if (number > 0)
+            cout << number << endl;
+        else
+            cout << -1 * number << endl;
+    }
+    return 0;
+}
