e96f7f66bb7edb845e85f1418cb1ceee7a7c417b
Abuhena Rony <23054213+EngrAbuhena@users.noreply.github.com>
2022-04-06 07:44:27 +0600

    Create ForLoopFTW.cpp

---
+++ ForLoopFTW.cpp
@@ -0,0 +1,23 @@
+// For Loop FTW
+// Accepted
+// Author: Abuhena Rony
+#include <bits/stdc++.h>
+
+int main()
+{
+    int number = 0;
+    int max = 0;
+    scanf("%d", &number);
+    for (int x = 1; x <= 2; x++)
+    {
+        for (int y = number; y >= number; y--)
+        {
+            if (x * y == number and x + y > max)
+            {
+                max = x + y;
+            }
+        }
+    }
+    printf("%d", max);
+    return 0;
+}
