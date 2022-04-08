75c69994174cb7ec3e3dd1240fe4c246dfe4087a
Abuhena Rony <23054213+EngrAbuhena@users.noreply.github.com>
2022-04-09 00:35:33 +0600

    Create MakingFriends.cpp

---
+++ MakingFriends.cpp
@@ -0,0 +1,21 @@
+// Making Friends
+//
+// Author: Abuhena Rony
+#include <bits/stdc++.h>
+
+int main()
+{
+    long long int number, x, y = 0;
+    char num2[1000001];
+    scanf("%lld", &number);
+    for (x = 1; x < number; x++)
+    {
+        if (number % x == 0)
+        {
+            num2[y] = x;
+            y++;
+        }
+    }
+    printf("%lld\n", y);
+    return 0;
+}
