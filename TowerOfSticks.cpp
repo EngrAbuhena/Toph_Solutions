0c02baa17072eca36e86c3eca89925673cfbbca9
Abuhena Rony <23054213+EngrAbuhena@users.noreply.github.com>
2022-04-08 15:13:02 +0600

    Create TowerOfSticks.cpp

---
+++ TowerOfSticks.cpp
@@ -0,0 +1,44 @@
+// Tower Of Sticks
+// Accepted
+// Author: Abuhena Rony
+#include <bits/stdc++.h>
+
+int main()
+{
+    while(true)
+    {
+        int a, b;
+        scanf("%d %d", &a, &b);
+        if (a > b)
+        {
+            int count = 0;
+            while (a > b)
+            {
+                a -= 1;
+                b += 1;
+                count++;
+            }
+            if (count > 0 and a == b)
+                printf("%d\n", count);
+            else
+                printf("impossible\n");
+        }
+        else if (a < b)
+        {
+            int count = 0;
+            while (a < b)
+            {
+                a += 1;
+                b -= 1;
+                count++;
+            }
+            if (count > 0 and a == b)
+                printf("%d\n", count);
+            else
+                printf("impossible\n");
+        }
+        else
+            break;
+    }
+    return 0;
+}
