5b619f922ed17cfe67ec4780de90f0cec223eb34
Abuhena Rony <23054213+EngrAbuhena@users.noreply.github.com>
2022-04-20 13:07:53 +0600

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,101 +1,29 @@
-// Formatted Numbers
-// Accepted
+// Set Union
+//
 // Author @ Abuhena Rony
 #include <bits/stdc++.h>
+using namespace std;
 
 int main()
 {
-    char number[10];
-    // printf("%d\n", strlen(number)); // length of the string
-    //   scanf("%s", number);
-    fgets(number, sizeof(number), stdin); // read string
+    int n1, n2, a;
+    set<int, less<int>> number;
 
-    if (strlen(number) == 4)
-    {
-        printf("%c", number[0]);
-        printf(",");
-        printf("%c", number[1]);
-        printf("%c", number[2]);
-        printf("%c", number[3]);
-    }
-    else if (strlen(number) == 5)
-    {
-        printf("%c", number[0]);
-        printf("%c", number[1]);
-        printf(",");
-        printf("%c", number[2]);
-        printf("%c", number[3]);
-        printf("%c", number[4]);
-    }
-    else if (strlen(number) == 6)
-    {
-        printf("%c", number[0]);
-        printf("%c", number[1]);
-        printf("%c", number[2]);
-        printf(",");
-        printf("%c", number[3]);
-        printf("%c", number[4]);
-        printf("%c", number[5]);
-    }
-    else if (strlen(number) == 7)
-    {
-        printf("%c", number[0]);
-        printf(",");
-        printf("%c", number[1]);
-        printf("%c", number[2]);
-        printf("%c", number[3]);
-        printf(",");
-        printf("%c", number[4]);
-        printf("%c", number[5]);
-        printf("%c", number[6]);
-    }
-    else if (strlen(number) == 8)
-    {
-        printf("%c", number[0]);
-        printf("%c", number[1]);
-        printf(",");
-        printf("%c", number[2]);
-        printf("%c", number[3]);
-        printf("%c", number[4]);
-        printf(",");
-        printf("%c", number[5]);
-        printf("%c", number[6]);
-        printf("%c", number[7]);
-    }
-    else if (strlen(number) == 9)
-    {
-        printf("%c", number[0]);
-        printf("%c", number[1]);
-        printf("%c", number[2]);
-        printf(",");
-        printf("%c", number[3]);
-        printf("%c", number[4]);
-        printf("%c", number[5]);
-        printf(",");
-        printf("%c", number[6]);
-        printf("%c", number[7]);
-        printf("%c", number[8]);
-    }
-    else if (strlen(number) == 10)
-    {
-        printf("%c", number[0]);
-        printf(",");
-        printf("%c", number[1]);
-        printf("%c", number[2]);
-        printf("%c", number[3]);
-        printf(",");
-        printf("%c", number[4]);
-        printf("%c", number[5]);
-        printf("%c", number[6]);
-        printf(",");
-        printf("%c", number[7]);
-        printf("%c", number[8]);
-        printf("%c", number[9]);
-    }
-    else
-        printf("%s", number);
+    scanf("%d %d", &n1, &n2);
 
-    // printf("%s", number);
-    // puts(number); // display string
+    int input, num3 = n1 + n2;
+
+    for (a = 0; a < num3; ++a)
+    {
+        scanf("%d", &input);
+        number.insert(input);
+    }
+    
+    auto it = number.begin();
+    printf("%d", *it);
+    for (it++; it != number.end(); it++)
+        printf(" %d", *it);
+    printf("\n");
+
     return 0;
 }
