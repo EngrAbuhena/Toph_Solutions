d5c4ca1d8d399dbc23c799fb7c9f37bcecdfd216
Abuhena Rony <23054213+EngrAbuhena@users.noreply.github.com>
2022-04-21 15:49:41 +0600

    Create ASCIIProgressBar.cpp

---
+++ ASCIIProgressBar.cpp
@@ -0,0 +1,34 @@
+// ASCII Progress Bar
+// Accepted
+// Author @ Abuhena Rony
+#include <bits/stdc++.h>
+
+void ASCII_Progress_Bar(double perc)
+{
+    int per = floor(perc);
+    int limit = per / 10;
+    char percent = '%';
+    char brac1 = '[';
+    char brac2 = ']';
+    printf("%c", brac1);
+    for (int x = 1; x <= 10; x++)
+    {
+        if (x <= limit)
+            printf("+");
+        else if (x > limit)
+            printf(".");
+        else
+            break;
+    }
+    printf("%c", brac2);
+    printf(" %d", per);
+    printf("%c", percent);
+}
+
+int main()
+{
+    double percent;
+    scanf("%lf", &percent);
+    ASCII_Progress_Bar(percent);
+    return 0;
+}
