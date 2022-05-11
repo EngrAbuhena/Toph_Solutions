66e1adda3df8fab1a5d9c69f4f16ed001084e426
Abuhena Rony <23054213+EngrAbuhena@users.noreply.github.com>
2022-05-11 21:26:06 +0600

    Update PascalsTriangle.cpp

--- PascalsTriangle.cpp
+++ PascalsTriangle.cpp
@@ -8,7 +8,7 @@ int main()
     int row;           // how much row do we have
     scanf("%d", &row); // read input for rows
 
-    long int sum = pow(2, row - 1); // sum of every elements of that row is 2 to the power n
+    long int sum = pow(2, row - 1); // sum of every elements of that row is 2 to the power n-1
 
     if (row > 0 and row < 30) // check if the row is less than 30
         printf("%ld", sum);   // print sum
