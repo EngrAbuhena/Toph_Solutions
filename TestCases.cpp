66ac4aee1412abec1f26db04b5d889bfc6966cd8
Abuhena Rony <23054213+EngrAbuhena@users.noreply.github.com>
2022-05-11 21:26:00 +0600

    Create TestCases.cpp

---
+++ TestCases.cpp
@@ -0,0 +1,37 @@
+// Test Cases
+// Wrong answer on test 5
+// Author @ Abuhena Rony
+#include <bits/stdc++.h>
+
+int main()
+{
+    int cases, cpul, meml;
+    scanf("%d %d %d", &cases, &cpul, &meml);
+    int diff, cpu, mem;
+
+    bool wa = false, cle = false, mle = false;
+    for (size_t i{1}; i <= cases; i++)
+    {
+        scanf("%d %d %d", &diff, &cpu, &mem);
+
+        if (cpu > cpul)
+            cle = true;
+        else if (mem > meml)
+            mle = true;
+        else if (diff > 0)
+            wa = true;
+        else
+            continue;
+    }
+
+    if (cle)
+        printf("CLE\n");
+    else if (mle)
+        printf("MLE\n");
+    else if (wa)
+        printf("WA\n");
+    else
+        printf("AC\n");
+
+    return 0;
+}
