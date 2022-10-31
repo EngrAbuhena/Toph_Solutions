0fd358b35ad8a9a15970c48d81c001d9027ad5db
Abuhena Rony <23054213+EngrAbuhena@users.noreply.github.com>
2022-10-31 23:41:01 +0600

    Create TheBoatFilledWithLove.cpp

---
+++ TheBoatFilledWithLove.cpp
@@ -0,0 +1,13 @@
+// The Boat Filled With Love
+// Accepted
+// Author @ Abuhena Rony
+
+#include <bits/stdc++.h>
+
+int main()
+{
+    int we;
+    scanf("%d", &we);
+    printf("We Love You. We %d Are Here For You.\n", we);
+    return 0;
+}
